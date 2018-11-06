80466a17f474777d0c3a578d9288ea98c1b76b6b
Morgana Sartor <morgana.sartor@outlook.com>
2018-11-06 15:41:32 -0200

    TESTE RELE

---
+++ teste-rele-temp.ino
@@ -0,0 +1,32 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define rele 7
+ 
+OneWire pino(3);
+DallasTemperature barramento(&pino);
+DeviceAddress sensor;
+ 
+void setup(void)
+{
+  Serial.begin(9600);
+  barramento.begin();
+  barramento.getAddress(sensor, 0);
+  pinMode( rele, OUTPUT);
+}
+ 
+void loop()
+{
+  float temperatura = 29.0;
+  Serial.print(temperatura);
+  
+  barramento.requestTemperatures(); 
+  float tempAtual =  barramento.getTempC(sensor);
+  Serial.print(tempAtual);
+  Serial.print("\n");
+  delay(500);
+  if(tempAtual <= temperatura){
+    digitalWrite(rele,HIGH);
+  }else{
+    digitalWrite(rele, LOW);
+}
+}
